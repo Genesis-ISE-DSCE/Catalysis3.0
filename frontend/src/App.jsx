7b8ee4389d9a27da90648e6feb8b7adc4186bb84
Denyme24 <namanraj24@outlook.com>
2025-03-04 20:58:26 +0530

    feat:event page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Hero } from './components/Hero'
-import { Schedule } from './components/Schedule'
-import { About } from './components/About'
-import { Gallery } from './components/Gallery'
-import { Events } from './components/Events'
-import { FAQ } from './components/FAQ'
-import { Contact } from './components/Contact'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import { Hero } from "./components/Hero";
+import { Schedule } from "./components/Schedule";
+import { About } from "./components/About";
+import { Gallery } from "./components/Gallery";
+import { Events } from "./components/Events";
+import { FAQ } from "./components/FAQ";
+import { Contact } from "./components/Contact";
 
 function App() {
-//use tailwind maybe? 
+  //use tailwind maybe?
   return (
     <>
       <div>
-        <h1 className='text-xl font-bold text-red-300'>Catalysis3.0</h1>
-        <Hero/>
-        <About/>
-        <Events/>
-        <Schedule/>
-        <Gallery/>
-        <FAQ/>
-        <Contact/>
+        <h1 className="text-xl  font-bold text-red-300">Catalysis3.0</h1>
+        <Hero />
+        <About />
+        <Events />
+        <Schedule />
+        <Gallery />
+        <FAQ />
+        <Contact />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
