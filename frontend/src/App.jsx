8b912ef2a947b1a5cd421e0261108ee1f2db6e9a
Denyme24 <namanraj24@outlook.com>
2025-03-05 08:29:51 +0530

    fixed some merge conflict issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import { Hero } from "./components/Hero";
 import { Schedule } from "./components/Schedule";
 import { About } from "./components/About";
 import { Gallery } from "./components/Gallery";
 import { Events } from "./components/Events";
-import { FAQ } from "./components/FAQ";
+import { FAQSection } from "./components/FAQ";
 import { Contact } from "./components/Contact";
+import "@fontsource/bangers";
 
 function App() {
   //use tailwind maybe?
   return (
     <>
       <div>
-        <h1 className="text-xl  font-bold text-red-300">Catalysis3.0</h1>
+        <h1 className="text-5xl font-bold text-red-500 font-comic">
+          Catalysis3.0
+        </h1>
         <Hero />
         <About />
         <Events />
         <Schedule />
         <Gallery />
-        <FAQ />
+        <FAQSection />
         <Contact />
       </div>
     </>
