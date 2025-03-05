08cb1c14ce0436a049cb94b92dbda045bd9b8033
sohan10012 <sohan10012006@gmail.com>
2025-03-06 00:52:58 +0530

    Gallery section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { Hero } from "./components/Hero";
 import { Schedule } from "./components/Schedule";
 import { About } from "./components/About";
-import { Gallery } from "./components/Gallery";
 import { Events } from "./components/Events";
 import { FAQSection } from "./components/FAQ";
 import { Contact } from "./components/Contact";
+import Gallery from "/src/components/Gallery";
 import "@fontsource/bangers";
 
 function App() {
