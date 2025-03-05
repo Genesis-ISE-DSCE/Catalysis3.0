fabf0178efc3486fc5ba5498ac055fb5d3659dac
Sohan <166704949+sohan10012@users.noreply.github.com>
2025-03-06 01:03:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Hero } from './components/Hero'
 import { Schedule } from './components/Schedule'
 import { About } from './components/About'
-import  Gallery  from './components/Gallery'
+import Gallery from './components/Gallery'
 import { Events } from './components/Events'
-import { FAQSection} from './components/FAQ'
+import { FAQSection } from './components/FAQ'
 import { Contact } from './components/Contact'
-import { Register } from './components/Register'
 import '@fontsource/bangers';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
