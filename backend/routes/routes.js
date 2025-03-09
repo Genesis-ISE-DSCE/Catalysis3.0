410d3e231d9d72a429756ded37d56fcbe545ca77
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-03-09 15:29:40 +0530

    [feat]:added Contact API
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const contactHandler = require('../handler/contact');
+const contactHandler = require('../handler/contact');
 const eventHandler = require('../handler/eventHandler');
 
 const router = express.Router();
 
 // Contact Us route
-// router.post('/contact', contactHandler.sendMessage);
+router.post('/contact', contactHandler.sendMessage);
 
 // Event Registration route
 router.post('/register', eventHandler.registerForEvent);
