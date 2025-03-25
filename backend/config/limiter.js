be99280135ca6ddde0e6031d1c6149c824dd30eb
Genesis <168651371+isegenesis@users.noreply.github.com>
2025-03-25 22:37:43 +0530

    Update limiter.js 2

--- backend/config/limiter.js
+++ backend/config/limiter.js
@@ -1,8 +1,9 @@
 const { rateLimit } = require("express-rate-limit");
 
-export const limiter = rateLimit({
+ const limiter = rateLimit({
   windowMs: 100 * 60 * 1000,
   limit: 4,
   standardHeaders: "draft-7",
   legacyHeaders: false,
 });
+module.exports = limiter;
