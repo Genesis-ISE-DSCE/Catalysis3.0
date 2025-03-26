dcf2298b03f52ee982d184ada1ab376bf4f688d6
Genesis <168651371+isegenesis@users.noreply.github.com>
2025-03-26 16:11:18 +0530

    Update routes.js6

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const contactHandler = require('../handler/contact');
 const eventHandler = require('../handler/eventHandler');
-const limiter = require('../config/limiter')
+// const limiter = require('../config/limiter')
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/contact', contactHandler.sendMessage);
 
 // Event Registration route
-router.post('/register',limiter,eventHandler.registerForEvent);
+router.post('/register',eventHandler.registerForEvent);
 
 // Get all event registrations
 router.get('/events', eventHandler.getAllRegistrations);
