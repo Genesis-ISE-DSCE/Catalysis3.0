b0577a753e660e9db265245e01a54f3d0e2f0e4c
Genesis <168651371+isegenesis@users.noreply.github.com>
2025-03-27 10:27:54 +0530

    deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { Contact } from './components/Contact'
 import { Admin } from './components/Admin'
 import '@fontsource/bangers';
 import { Route,BrowserRouter as Router,Routes, Navigate } from 'react-router-dom'
-// import { Register } from './components/Register'
+// import { Register } from './components/Register' registration closed
 import Navbar from './components/Navbar'
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { Login } from './components/Login';
